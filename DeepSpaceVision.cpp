252323cdaa8fde3a346a3e11a8f5a24ee5c05091
RyanBoring <boring.ryan.alan@gmail.com>
2019-02-19 12:22:36 -0500

    Add functionality for second camera devoted to streaming

--- DeepSpaceVision.cpp
+++ DeepSpaceVision.cpp
@@ -16,7 +16,8 @@ int contourPolygonAccuracy{5};
 int minArea{60},
 	minRotation{45};
 
-int videoSource{1};
+int processingVideoSource{1},
+	viewingVideoSource{2};
 
 std::string udpHost{"10.28.51.2"};
 int udpSendPort{9000}, udpReceivePort{9001};
@@ -28,46 +29,30 @@ int bitrate{60000};
 std::string videoHost{"10.28.51.210"};
 int videoPort{9001};
 
-void actuallySeeFlash()
+void transmitVideo()
 {
-	//Makes sure the camera is set to its optimal settings for actually seeing what's going on
-	system("v4l2-ctl -d /dev/video1 \
-		--set-ctrl brightness=150 \
-		--set-ctrl contrast=25 \
-		--set-ctrl saturation=30 \
-		--set-ctrl white_balance_temperature_auto=0 \
-		--set-ctrl white_balance_temperature=50 \
-		--set-ctrl power_line_frequency=2 \
-		--set-ctrl sharpness=50 \
-		--set-ctrl exposure_auto=1 \
-		--set-ctrl exposure_absolute=50");
-}
-
-void detectionFlash()
-{
-	//Flashes the camera with optimal settings for identifying the targets
-	system("v4l2-ctl -d /dev/video1 \
-		--set-ctrl brightness=100 \
-		--set-ctrl contrast=0 \
-		--set-ctrl saturation=100 \
-		--set-ctrl white_balance_temperature_auto=0 \
-		--set-ctrl white_balance_temperature=9000 \
-		--set-ctrl power_line_frequency=2 \
-		--set-ctrl sharpness=24 \
-		--set-ctrl exposure_auto=1 \
-		--set-ctrl exposure_absolute=5");
+	char buffer[500];
+	sprintf(buffer,
+		"gst-launch-1.0 -v v4l2src device=/dev/video%d ! "
+		"video/x-raw,format=I420,width=%d,height=%d,framerate=%d/1 ! "
+		"queue ! videoconvert ! omxh264enc control-rate=2 bitrate=%d ! "
+		"video/x-h264, stream-format=byte-stream ! h264parse ! "
+		"rtph264pay mtu=1400 ! udpsink host=%s port=%d sync=false async=false",
+		viewingVideoSource, width, height, framerate, bitrate, videoHost.c_str(), videoPort);
+		
+	system(buffer);
 }
 
 void extractContours(std::vector<std::vector<cv::Point>> &contours, cv::Mat frame, cv::Scalar &hsvLowThreshold, cv::Scalar &hsvHighThreshold, cv::Mat morphElement)
 {
-	cv::imshow("Base Image", frame);
+	//cv::imshow("Base Image", frame);
 
 	cv::cvtColor(frame, frame, cv::COLOR_BGR2HSV);
 
 	//Singles out the pixels that meet the HSV range of the target and displays them
 	cv::inRange(frame, hsvLowThreshold, hsvHighThreshold, frame);
 
-	cv::imshow("After inRange", frame);
+	//cv::imshow("After inRange", frame);
 
 	//Applies an open morph to the frame (erosion (dark spaces expand) followed by a dilation (light spaces expand) to remove small particles with a kernel specified by morphElement) and displays it
 	//cv::morphologyEx(frame, frame, cv::MORPH_OPEN, morphElement);
@@ -81,7 +66,7 @@ void extractContours(std::vector<std::vector<cv::Point>> &contours, cv::Mat fram
 	//Applies the Canny edge detection algorithm to extract edges
 	cv::Canny(frame, frame, 0, 0);
 
-	cv::imshow("Canny", frame);
+	//cv::imshow("Canny", frame);
 
 	//Finds the contours in the image and stores them in a vector of vectors of cv::Points (each vector of cv::Points represents the curve of the contour)
 	//CV_RETR_EXTERNAL specifies to only detect contours on the edges of particles
@@ -91,8 +76,6 @@ void extractContours(std::vector<std::vector<cv::Point>> &contours, cv::Mat fram
 
 int main()
 {
-	bool viewingMode{true};
-
 	cv::Mat frame;
 
 	double distanceTo{0},
@@ -104,6 +87,18 @@ int main()
 	UDPHandler udpHandler{udpHost, udpSendPort, udpReceivePort};
 
 	CvCapture_GStreamer camera;
+	
+	//Flashes the camera with optimal settings for identifying the targets
+	system("v4l2-ctl -d /dev/video1 \
+		--set-ctrl brightness=100 \
+		--set-ctrl contrast=0 \
+		--set-ctrl saturation=100 \
+		--set-ctrl white_balance_temperature_auto=0 \
+		--set-ctrl white_balance_temperature=9000 \
+		--set-ctrl power_line_frequency=2 \
+		--set-ctrl sharpness=24 \
+		--set-ctrl exposure_auto=1 \
+		--set-ctrl exposure_absolute=5");
 
 	//Creates an array of characters (acts like a string) to hold the split
 	//gstreamer pipeline. One half is sent to the Driver Station and the
@@ -112,125 +107,105 @@ int main()
 	sprintf(buffer,
 		"v4l2src device=/dev/video%d ! "
 		"video/x-raw,format=(string)I420,width=(int)%d,height=(int)%d,framerate=(fraction)%d/1 ! "
-		"tee name=split "
-		"split. ! queue ! videoconvert ! omxh264enc control-rate=2 bitrate=%d ! "
-		"video/x-h264, stream-format=(string)byte-stream ! h264parse ! "
-		"rtph264pay mtu=1400 ! udpsink host=%s port=%d sync=false async=false "
-		"split. ! queue ! autovideoconvert ! appsink",
-		videoSource, width, height, framerate, bitrate, videoHost.c_str(), videoPort);
-
-	//Tells the camera to start reading from the split pipeline
+		"queue ! autovideoconvert ! appsink",
+		processingVideoSource, width, height, framerate);
+		
+	//Tells the camera to start reading from the pipeline to process video
 	camera.open(CV_CAP_GSTREAMER_FILE, buffer);
-
-	actuallySeeFlash();
+	
+	//Creates a new thread in which we create a gstreamer pipeline that transmits video to the Driver Station
+	std::thread transmitVideoThread{transmitVideo};
 
 	while (true)
 	{
-		//The UDP receiver expects seven chars but gets six so it adds an extra E
-		if (udpHandler.getMessage() == "ENABLEE"
-			|| udpHandler.getMessage() == "ENABLE")
-		{
-			detectionFlash();
-			udpHandler.clearMessage();
-			viewingMode = false;
-			std::cout << "Enabled\n";
-		}
-		else if (udpHandler.getMessage() == "DISABLE")
-		{
-			actuallySeeFlash();
-			udpHandler.clearMessage();
-			viewingMode = true;
-			std::cout << "Disabled\n";
-		}
-
 		//Gets the next frame from the camera
 		//This returns true if it was successful
 		while (!camera.grabFrame())
 		{
 			std::cout << "Could not retrieve frame! Please check camera connection. Trying again...\n";
 		}
-
-		if (!viewingMode)
+		
+		IplImage *img = camera.retrieveFrame(0);
+		frame = cv::cvarrToMat(img);
+		
+		cv::imshow("Frame", frame);
+		
+		std::vector<std::vector<cv::Point>> contoursRaw;
+		extractContours(contoursRaw, frame, hsvLow, hsvHigh, morphElement);
+		std::vector<Contour> contours(contoursRaw.size());
+		for (int i{0}; i < contoursRaw.size(); ++i)
 		{
-			IplImage *img = camera.retrieveFrame(0);
-			frame = cv::cvarrToMat(img);
+			contours.at(i) = Contour(contoursRaw.at(i));
+		}
 
-			std::vector<std::vector<cv::Point>> contoursRaw;
-			extractContours(contoursRaw, frame, hsvLow, hsvHigh, morphElement);
-			std::vector<Contour> contours(contoursRaw.size());
-			for (int i{0}; i < contoursRaw.size(); ++i)
+		//Filters out bad contours and adds the dimensions of the good contours to the above vectors
+		for (int c{0}; c < contours.size(); ++c)
+		{
+			if (!contours.at(c).isValid(minArea, minRotation, 3))
 			{
-				contours.at(i) = Contour(contoursRaw.at(i));
+				contours.erase(contours.begin() + contours.size() - 1);
+				--c;
+				continue;
 			}
+		}
 
-			//Filters out bad contours and adds the dimensions of the good contours to the above vectors
-			for (int c{0}; c < contours.size(); ++c)
+		//Least distant contour initialized with -1 so it's not confused for an actual contour and can be tested for not being valid
+		int leastDistantContour{-1};
+
+		//Now that we've identified compliant targets, we find their match (if they have one)
+		for (int origContour{0}; origContour < contours.size(); ++origContour)
+		{
+			//We identify the left one first because why not
+			if (contours.at(origContour).angle > 0)
 			{
-				if (!contours.at(c).isValid(minArea, minRotation, 3))
+				//Iterates through all of the contours and compares them against the original
+				for (int compareContour{0}; compareContour < contours.size(); ++compareContour)
 				{
-					contours.erase(contours.begin() + contours.size() - 1);
-					--c;
-					continue;
-				}
-			}
-
-			//Least distant contour initialized with -1 so it's not confused for an actual contour and can be tested for not being valid
-			int leastDistantContour{-1};
-
-			//Now that we've identified compliant targets, we find their match (if they have one)
-			for (int origContour{0}; origContour < contours.size(); ++origContour)
-			{
-				//We identify the left one first because why not
-				if (contours.at(origContour).angle > 0)
-				{
-					//Iterates through all of the contours and compares them against the original
-					for (int compareContour{0}; compareContour < contours.size(); ++compareContour)
+					//If the contour to compare against isn't the original
+					//and the contour is angled left
+					//and the contour is right of the original
+					//and (if the least distant contour hasn't been set
+					//OR this contour is closer than the last least distant contour)
+					//then this contour is the new least distant contour
+					if (compareContour != origContour && contours.at(compareContour).angle < 0 && contours.at(origContour).rotatedBoundingBoxPoints[0].x < contours.at(compareContour).rotatedBoundingBoxPoints[0].x)
 					{
-						//If the contour to compare against isn't the original
-						//and the contour is angled left
-						//and the contour is right of the original
-						//and (if the least distant contour hasn't been set
-						//OR this contour is closer than the last least distant contour)
-						//then this contour is the new least distant contour
-						if (compareContour != origContour && contours.at(compareContour).angle < 0 && contours.at(origContour).rotatedBoundingBoxPoints[0].x < contours.at(compareContour).rotatedBoundingBoxPoints[0].x)
+						//We test if it's closer to the original contour after checking if the
+						//index is negative since passing a negative number to a vector will
+						//throw an OutOfBounds exception
+						if (leastDistantContour == -1)
 						{
-							//We test if it's closer to the original contour after checking if the
-							//index is negative since passing a negative number to a vector will
-							//throw an OutOfBounds exception
-							if (leastDistantContour == -1)
-							{
-								leastDistantContour = compareContour;
-							}
-							else if (contours.at(compareContour).rotatedBoundingBoxPoints[0].x - contours.at(origContour).rotatedBoundingBoxPoints[0].x < contours.at(leastDistantContour).rotatedBoundingBoxPoints[0].x)
-							{
-								leastDistantContour = compareContour;
-							}
+							leastDistantContour = compareContour;
+						}
+						else if (contours.at(compareContour).rotatedBoundingBoxPoints[0].x - contours.at(origContour).rotatedBoundingBoxPoints[0].x < contours.at(leastDistantContour).rotatedBoundingBoxPoints[0].x)
+						{
+							leastDistantContour = compareContour;
 						}
 					}
+				}
 
-					//If we found the second contour, calculate its position in relation to us
-					if (leastDistantContour != -1)
-					{
-						//For clarity
-						double origCenterX = contours.at(origContour).rotatedBoundingBox.center.x,
-							   origCenterY = contours.at(origContour).rotatedBoundingBox.center.y;
-						double leastDistantX = contours.at(leastDistantContour).rotatedBoundingBox.center.x,
-							   leastDistantY = contours.at(leastDistantContour).rotatedBoundingBox.center.y;
+				//If we found the second contour, calculate its position in relation to us
+				if (leastDistantContour != -1)
+				{
+					//For clarity
+					double origCenterX = contours.at(origContour).rotatedBoundingBox.center.x,
+						   origCenterY = contours.at(origContour).rotatedBoundingBox.center.y;
+					double leastDistantX = contours.at(leastDistantContour).rotatedBoundingBox.center.x,
+						   leastDistantY = contours.at(leastDistantContour).rotatedBoundingBox.center.y;
 
-						//The original contour will always be the left one since that's what we've specified
-						//Calculates and spits out some values for us
-						//distanceTo = (whatever calculation);
-						horizontalAngleError = -((frame.cols / 2.0) - (origCenterX + ((leastDistantX - origCenterX) / 2))) / frame.cols * fovAngle;
-						verticalAngleError = ((frame.rows / 2.0) - (origCenterY + ((leastDistantY - origCenterY) / 2))) / frame.rows * fovAngle;
+					//The original contour will always be the left one since that's what we've specified
+					//Calculates and spits out some values for us
+					//distanceTo = (whatever calculation);
+					horizontalAngleError = -((frame.cols / 2.0) - (origCenterX + ((leastDistantX - origCenterX) / 2))) / frame.cols * fovAngle;
+					verticalAngleError = ((frame.rows / 2.0) - (origCenterY + ((leastDistantY - origCenterY) / 2))) / frame.rows * fovAngle;
 
-						udpHandler.send(std::to_string(horizontalAngleError));
-					}
+					udpHandler.send(std::to_string(horizontalAngleError));
 				}
 			}
-
-			//Allows us to see the frames we displayed with cv::imshow
-			cv::waitKey(1);
 		}
+			
+
+		//Allows us to see the frames we displayed with cv::imshow
+		cv::waitKey(1);
 	}
 
 	camera.close();
