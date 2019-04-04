7c3b837c64cca960ba9a089787a52d14ed32567f
Ryan Boring <boring.ryan.alan@gmail.com>
2019-04-03 23:00:46 -0400

    Fix duplicate cameras

--- DeepSpaceVision.cpp
+++ DeepSpaceVision.cpp
@@ -29,9 +29,6 @@ int processingVideoSource{0},
 CvCapture_GStreamer processingCamera;
 CvCapture_GStreamer viewingCamera;
 
-CvCapture_GStreamer processingCamera;
-CvCapture_GStreamer viewingCamera;
-
 std::string udpHost{"10.28.51.2"};
 int udpSendPort{9000}, udpReceivePort{9001};
 
